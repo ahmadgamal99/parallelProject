9ed2d9e66fa36a0ff8dcab8ac4c6f00d9ae77862
ahmadgamal99 <60099865+ahmadgamal99@users.noreply.github.com>
2021-03-16 23:12:46 +0200

    .

---
+++ reactive_app/lib/screens/view_data.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:reactive_app/widgets/news_wiget.dart';
+
+class ViewData extends StatelessWidget {
+  final rows;
+
+  ViewData({this.rows});
+
+  @override
+  Widget build(BuildContext context) {
+    print(rows.length);
+    return Scaffold(
+      body: Visibility(
+        visible: rows.length > 0 ,
+        child: ListView.builder(
+          itemCount: rows.length,
+          itemBuilder: (context, i) {
+            return NewsWidget(
+              title: rows[i]['title'],
+              author: rows[i]['author'],
+              description: rows[i]['description'],
+              publishedAt: rows[i]['publishedAt'],
+              source: rows[i]['source'],
+            );
+          },
+        ),
+        replacement: Center(
+          child: Text("There is No Data Yet"),
+        ),
+      )
+    );
+  }
+}
